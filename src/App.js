1b6545b0273c9135b7d569be22f6d542212e7f45
triptotest01 <triptotest01@gmail.com>
2023-03-12 13:34:46 +0900

    useReducer 예제 : X, O 게임 구현

--- src/App.js
+++ src/App.js
@@ -1,3 +1,25 @@
+import React from 'react';
+import Game from './Game';
+
+function App(){
+
+  return(
+    <Game/>
+  )
+}
+
+export default App
+
+
+
+
+
+
+
+
+
+
+
 // import React from "react";
 // import ReducerCounter from "./ReducerCounter";
 
